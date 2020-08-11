6f024e20bf0c901b6462435379100a1d8d4d9ac6
Dennis <dmnickolai@gmail.com>
2020-08-11 09:16:18 -0700

    , no implementations

--- src/processphotodups/DupPhotoRecord.java
+++ src/processphotodups/DupPhotoRecord.java
@@ -10,5 +10,49 @@ package processphotodups;
  * @author Dennis
  */
 public class DupPhotoRecord {
+    // Constants
+    DupDbHelper myDbHelper;
+
+// Constructor receives instance of DupsDbHelper
+    DupPhotoRecord( DupDbHelper theDbHelper) {
+        myDbHelper = theDbHelper;
     
+}
+    
+    void getSomeDupRecords(int limit) {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    long getId() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    String getTimeStamp() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    String getDupFilePath() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    String getBaseFilePath() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    void markDelete() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    void markSkipped() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    boolean moveToNext() {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }
+    
+    private void getMoreRecords()
+    {
+        throw new UnsupportedOperationException("dbSelect with limit not supported yet.");
+    }    
 }
