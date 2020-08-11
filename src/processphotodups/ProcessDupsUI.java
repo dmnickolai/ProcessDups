8eede105a391aa86a71ace1a4c4a112b06793d10
Dennis <dmnickolai@gmail.com>
2020-08-11 12:56:44 -0700

    Photo Panel mostly done Add no photo to icon if cannot find file

--- src/processphotodups/ProcessDupsUI.java
+++ src/processphotodups/ProcessDupsUI.java
@@ -7,167 +7,121 @@
  */
 package processphotodups;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.MediaTracker;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.border.LineBorder;
+
+
 
 /**
  *
  * @author Dennis Nickolai
  */
-public class ProcessDupsUI {
-
-/**
- *
- * @author Dennis
- */
-public class DupDbHelper {
+public class ProcessDupsUI extends JFrame{
     
-    //  Constants necessary to open DB Connection
-    static final String dbDriver = "com.mysql.cj.jdbc.Driver";
-    static final String dbName = "photos_catalog";
-    static final String dbUrl = "jdbc:mysql://localhost:3306/" + dbName;
-    static final String dbUser ="dennis";
-    static final String dbPassword = "55555";
-    
-    // Class Variables
-    Connection dbConn = null;
-    ResultSet dbRows = null;
-     
-    String baseTableName = "testBase";
-    String dupsTableName = baseTableName + "Dups";
-    
-    String selectBaseSQL = "SELECT * FROM " + baseTableName + " WHERE timestamp = ?;";
-    PreparedStatement selectBase = null;
-    
-    String selectDupSQL = "SELECT * FROM " + dupsTableName + 
-            " WHERE skipped = 0 AND markedfordelete = 0 " +
-            " LIMIT ?;";
-    PreparedStatement selectDups = null;
-    
-    ResultSet allDups = null;
-    
-    //ResultSet dupsTable = null;
-    
-    DupDbHelper() {
-        dbConn = getDbConnection();
-        try {        
-            selectBase = dbConn.prepareStatement(selectBaseSQL);  
-            System.out.println(selectBase);
-            System.out.println(selectDupSQL);
-            selectDups = dbConn.prepareCall(selectDupSQL, ResultSet.TYPE_SCROLL_SENSITIVE, 
-                        ResultSet.CONCUR_UPDATABLE);
-            System.out.println(selectDups);
-        }
-        catch (Exception ex) {
-            System.out.println("Error in Initialize:" + ex.getMessage());
-            System.exit(2);
-        }  
-    }
-    
-    ResultSet getDupDbEntries (int limit){
-        allDups = null;
-        int rowCount = 0;
+   Container mainContainer = null;
+   private ProcessDupsUI me = null; 
+   ResultSet dupsTable = null;
+   Icon noPhoto = null;
+   
+   //DupDbHelper dbHelper = new DupDbHelper();
+   PhotoPanel basePhotoPanel = null;
+   PhotoPanel dupPhotoPanel = null;
+   
+   public ProcessDupsUI( ActionListener baseListener, ActionListener dupListener) {
+        super("Process Photos with Same Timestamp");
         
-        try {
-            selectDups.setInt(1, limit);
-            allDups = selectDups.executeQuery();
-            if (allDups.last()) {//make cursor to point to the last row in the ResultSet object
-                 rowCount = allDups.getRow();
-                 allDups.beforeFirst(); //make cursor to point to the front of the ResultSet object, just before the first row.
-               }
-            System.out.println("Total number of rows in ResultSet object = "+rowCount);
-        } catch (SQLException ex) {
-             Logger.getLogger(DupDbHelper.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Exception in Dups:" + ex.getMessage());
-            System.exit(7);
-        }
-        return allDups;
+        initClass();        
+        buildUI(baseListener, dupListener);   
     }
-    
-    int getResultSetSize (ResultSet rs) {
-        int rowCount = 0;
-        try {
-            if (rs.last()) {//make cursor to point to the last row in the ResultSet object
-                rowCount = allDups.getRow();
-                allDups.beforeFirst(); //make cursor to point to the front of the ResultSet object, just before the first row.
+   
+   private void buildUI(ActionListener baseListener, ActionListener dupListener) {
+        // Setup full window
+        mainContainer.setLayout(new BorderLayout(8,6));
+        mainContainer.setBackground(Color.YELLOW);
+        getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4,Color.GREEN ));
+        // Build and add title panel
+        JPanel topPanel = new JPanel();
+        topPanel.setBorder(new LineBorder(Color.BLACK,3));
+        topPanel.setBackground(Color.ORANGE);
+        JLabel topLine = new JLabel("My New App");
+        topLine.setOpaque(true);
+        topLine.setAlignmentX(Component.CENTER_ALIGNMENT);
+        topLine.setHorizontalTextPosition(JLabel.CENTER);
+        topPanel.add(topLine, BorderLayout.NORTH);
+        add(topPanel, BorderLayout.NORTH);
+        // Build working panel showing Base and Dup photos
+        JPanel middlePanel = new JPanel();
+        middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
+        
+        // Event handler for Base Panel buttons
+        basePhotoPanel = new PhotoPanel("Base Photo","", baseListener);
+        basePhotoPanel.btnSkip.setVisible(false);
+          
+        add(basePhotoPanel,BorderLayout.CENTER);
+          middlePanel.add(basePhotoPanel);
+       
+        dupPhotoPanel = new PhotoPanel("Possible Duplicate","", dupListener);
+        middlePanel.add(dupPhotoPanel);
+        add(middlePanel,BorderLayout.CENTER);
+
+        JPanel p = new JPanel();
+        p.setBorder(new LineBorder(Color.BLACK,3));
+        p.setBackground(Color.WHITE);
+        p.add(new JLabel("South"));
+        add(p,BorderLayout.SOUTH);
+        
+   }
+   
+    private void initClass() {
+        //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setSize(1000,900);
+        //setLocationRelativeTo(null); 
+
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        me = this;
+        // Capture app closing to close DB connection
+        WindowListener exitListener = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                /*
+               if (dbHelper !=null) {
+                   dbHelper.closeDbConnection();
+                   
+
+                }
+*/
+            System.exit(0);
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(DupDbHelper.class.getName()).log(Level.SEVERE, null, ex);
-            System.exit(12);
-        }
-        return rowCount;
+        };
+         me = this;       
+        this.addWindowListener(exitListener);
+        mainContainer = getContentPane();
+       
     }
     
-    int dbUpdate () {
-        throw new Exception ("not implemented");
-    }
     
-    Connection getDbConnection() {
-       //if connection exists, return it
-       if (dbConn != null) return dbConn;
-       // open database connection
-       try { 
-           Class.forName(dbDriver);
-           Connection conn = DriverManager.getConnection(dbUrl, dbUser,dbPassword);
-           System.out.println("Connection OK");
-           dbConn = conn;
-           return conn;          
-       }
-       catch (SQLException ex){
-           System.out.println("SQL Exception:" + ex.getMessage());
-           System.out.println("Aborting app.....");
-           System.exit(1);
-       }
-       catch (Exception ex){
-           System.out.println(ex.getMessage() + ex.getCause().toString());
-           System.out.println("Aborting app.....");
-           System.exit(1);
-       }
-       return null;
-    }
-
-    public void closeDbConnection(){
-        try{
-            if (dbConn!= null)
-                dbConn.close();
-        }
-        catch(SQLException ex) {
-            // report error then ignore
-            System.out.println("Exception in closs db connection, " + ex.getMessage());
-        }
-        finally {
-            dbConn = null;
-        }
-    }
-    
-    public void markDupSkipped() {
-        try {
-            int id = allDups.getInt("id");
-            PreparedStatement update = dbConn.prepareStatement("UPDATE " + dupsTableName + 
-                    " SET skipped = 1 WHERE id = ?");
-            update.setInt(1, id);
-            int j = update.getUpdateCount();
-            int count = update.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(DupDbHelper.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public void markToDelete() {
-        try {
-            int id = allDups.getInt("id");
-            PreparedStatement update = dbConn.prepareStatement("UPDATE " + dupsTableName + 
-                    " SET markedfordelete = 1 WHERE id = ?");
-            update.setInt(1, id);
-            int j = update.getUpdateCount();
-            int count = update.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(DupDbHelper.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
