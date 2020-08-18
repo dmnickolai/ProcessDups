d20bece63bbe8c97e3901ee3daac55a39276f5b3
Dennis <dmnickolai@gmail.com>
2020-08-18 15:33:45 -0700

    Complete and minor cleanup

--- src/processphotodups/DupPhotoRecordMgr.java
+++ src/processphotodups/DupPhotoRecordMgr.java
@@ -5,12 +5,19 @@
  */
 package processphotodups;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  *
@@ -21,22 +28,42 @@ public class DupPhotoRecordMgr {
     private static final Logger LOGGER
             = Logger.getLogger(DupPhotoRecordMgr.class.getName());
     // Constants
+    private static final int limit = 10;  // number of records to retrieve
+    private static final boolean whereDeleted = false;
+    private static final boolean whereSkipped = false;
+    private static final String logFileFolder = "K:\\Deleted from App";
+    private static final String logFilePath = logFileFolder + "\\appLog.log";
+    // Class varibles
     private DupDbHelper myDbHelper;
     private ResultSet currentRS;
     private long mostRecentID;
-    private int limit = 10;  // number of records to retrieve
+    private boolean includeMarked = false;
 
 // Constructor receives instance of DupsDbHelper
-    DupPhotoRecordMgr() {
-        Handler handlerObj = new ConsoleHandler();
-        handlerObj.setLevel(Level.ALL);
-        LOGGER.addHandler(handlerObj);
+    DupPhotoRecordMgr(boolean includeMarked) {
+        this.includeMarked = includeMarked;
+        Handler consoleHandlerObj = new ConsoleHandler();
+        consoleHandlerObj.setLevel(Level.ALL);
+        LOGGER.addHandler(consoleHandlerObj);
+        assureFolderExists(logFileFolder);
+        FileHandler fileHandlerObj = null;
+        try {
+            fileHandlerObj = new FileHandler(logFilePath);
+            fileHandlerObj.setFormatter(new SimpleFormatter());
+        } catch (IOException ex) {
+            Logger.getLogger(DupPhotoRecordMgr.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(DupPhotoRecordMgr.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        fileHandlerObj.setLevel(Level.ALL);
+        LOGGER.addHandler(fileHandlerObj);
         LOGGER.setLevel(Level.ALL);
         LOGGER.setUseParentHandlers(false);
-        LOGGER.log(Level.FINE, "In DbHelper Constuctor");
+        
+        LOGGER.log(Level.FINE, "In Record Manager Constuctor");
         
         myDbHelper = new DupDbHelper();
-        currentRS = myDbHelper.dbSelectWhere(limit, false, false);
+        currentRS = myDbHelper.dbSelect(limit, includeMarked);
         LOGGER.info(getRecordSetSize() + " rows from Select");
     }
 
@@ -98,13 +125,23 @@ public class DupPhotoRecordMgr {
     long getDupRecordCount() {
         return myDbHelper.getDupTableCount();
     }
+    
+    void deleteFromDupDb() {
+        myDbHelper.deleteDupRow(getId());
+    }
 
     void markDelete() {
+        logAction ("Delete");
         myDbHelper.markToDelete(getId());
     }
 
     void markSkipped() {
+        logAction ("Skipped");
         myDbHelper.markDupSkipped(getId());
+    }
+    
+    void logAction (String action){
+        LOGGER.info("ID=" + getId() + " marked as " + action);
     }
 
     private void getMoreRecords() {
@@ -122,6 +159,60 @@ public class DupPhotoRecordMgr {
             LOGGER.severe(ex.getMessage());
         }
         return -1;
+    }
+    
+    /**
+     * Assures that the folder exists by creating it if not found
+     *
+     * @param path folder path to validate or create
+     */
+    // Validates if folder exists or creates it if not
+    private void assureFolderExists(String path) {
+        File testFile = new File(path);
+        if (testFile.exists() && testFile.isDirectory()) {
+            return;
+        }
+        makeFolder(path);
+    }
+
+    //  Make folder whose path is input parameter
+    private void makeFolder(String path) {
+        //Creating a File object
+        File file = new File(path);
+        //Creating the directory
+        boolean result = file.mkdir();
+        if (!result) {
+            System.out.println("Couldn’t create specified directory: " + path);
+            System.exit(5);
+        }
+    }
+
+    private void moveBinFile(File sourceFile, String destFileString) {
+        byte[] buffer = null;
+        try {
+            //create FileInputStream object for source file
+            FileInputStream fin = new FileInputStream(sourceFile);
+            //create FileOutputStream object for destination file
+            FileOutputStream fout = new FileOutputStream(destFileString);
+            if (buffer == null) {
+                buffer = new byte[16000];
+            }
+            // copy file big hunk at at time
+            int noOfBytes = 0;
+
+            //read bytes from source file and write to destination file
+            while ((noOfBytes = fin.read(buffer)) != -1) {
+                fout.write(buffer, 0, noOfBytes);
+            }
+            //close the streams
+            fin.close();
+            fout.close();
+        } //end of try
+        catch (FileNotFoundException fnf) {
+            System.out.println("Specified file not found :" + fnf);
+        } catch (IOException ioe) {
+            System.out.println("Error while copying file :" + ioe);
+        }
     }
 
 }
