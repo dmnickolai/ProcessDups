8129cc03a73175d8fe6fb74b86ba84e7177ec41d
Dennis <dmnickolai@gmail.com>
2020-08-18 16:11:33 -0700

    Complete and minor cleanup

--- src/processphotodups/ProcessDupsUI.java
+++ src/processphotodups/ProcessDupsUI.java
@@ -34,51 +34,48 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.border.LineBorder;
 
-
-
 /**
+ * Performs the UI for the application. This class does not interact with the
+ * database or the dup record
  *
  * @author Dennis Nickolai
  */
-public class ProcessDupsUI extends JFrame{
-    
-   
-    
-   private Container mainContainer = null;
-   private ProcessDupsUI me = null; 
-   private ResultSet dupsTable = null;
-   private Icon noPhoto = null;
-   private JLabel lblStatus=null;
-   
-   //DupDbHelper dbHelper = new DupDbHelper();
-   private PhotoPanel basePhotoPanel = null;
-   private PhotoPanel dupPhotoPanel = null;
-   
-   private ActionButton[] baseButtons =  {ActionButton.REPLACE, ActionButton.RENAME, ActionButton.MOVE};
-   private ActionButton[] dupButtons =  {ActionButton.SKIP, ActionButton.DELETE, ActionButton.RENAME, ActionButton.MOVE};
-   
-   public ProcessDupsUI( ActionListener baseListener, ActionListener dupListener) {
+public class ProcessDupsUI extends JFrame {
+
+    private Container mainContainer = null;
+    private ProcessDupsUI me = null;
+    private ResultSet dupsTable = null;
+    private Icon noPhoto = null;
+    private JLabel lblStatus = null;
+
+    private PhotoPanel basePhotoPanel = null;
+    private PhotoPanel dupPhotoPanel = null;
+
+    private ActionButton[] baseButtons = {ActionButton.REPLACE, ActionButton.RENAME, ActionButton.MOVE};
+    private ActionButton[] dupButtons = {ActionButton.SKIP, ActionButton.DELETE, ActionButton.RENAME, ActionButton.MOVE};
+
+    public ProcessDupsUI(ActionListener baseListener, ActionListener dupListener) {
         super("Process Photos with Same Timestamp");
-        initClass();        
-        buildUI(baseListener, dupListener);   
+        initClass();
+        buildUI(baseListener, dupListener);
     }
-   
-   void showBasePhoto(String filePath) {
-       basePhotoPanel.showPhoto(filePath);
-   }
-   
-   void showDupPhoto(String filePath) {
-       dupPhotoPanel.showPhoto(filePath);
-   }
-   
-   private void buildUI(ActionListener baseListener, ActionListener dupListener) {
+
+    void showBasePhoto(String filePath) {
+        basePhotoPanel.showPhoto(filePath);
+    }
+
+    void showDupPhoto(String filePath) {
+        dupPhotoPanel.showPhoto(filePath);
+    }
+
+    private void buildUI(ActionListener baseListener, ActionListener dupListener) {
         // Setup full window
-        mainContainer.setLayout(new BorderLayout(8,6));
+        mainContainer.setLayout(new BorderLayout(8, 6));
         mainContainer.setBackground(Color.YELLOW);
-        getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4,Color.GREEN ));
+        getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.GREEN));
         // Build and add title panel
         JPanel topPanel = new JPanel();
-        topPanel.setBorder(new LineBorder(Color.BLACK,3));
+        topPanel.setBorder(new LineBorder(Color.BLACK, 3));
         topPanel.setBackground(Color.ORANGE);
         JLabel topLine = new JLabel("My New App");
         topLine.setOpaque(true);
@@ -89,57 +86,50 @@ public class ProcessDupsUI extends JFrame{
         // Build working panel showing Base and Dup photos
         JPanel middlePanel = new JPanel();
         middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
-        
+
         // Event handler for Base Panel buttons
         basePhotoPanel = new PhotoPanel("Base Photo", baseButtons, baseListener);
-        
-          
-        add(basePhotoPanel,BorderLayout.CENTER);
+
+        add(basePhotoPanel, BorderLayout.CENTER);
         middlePanel.add(basePhotoPanel);
-       
+
         dupPhotoPanel = new PhotoPanel("Possible Duplicate", dupButtons, dupListener);
         middlePanel.add(dupPhotoPanel);
-        add(middlePanel,BorderLayout.CENTER);
+        add(middlePanel, BorderLayout.CENTER);
 
         JPanel p = new JPanel();
-        p.setBorder(new LineBorder(Color.BLACK,3));
+        p.setBorder(new LineBorder(Color.BLACK, 3));
         p.setBackground(Color.WHITE);
         lblStatus = new JLabel("Status");
         p.add(lblStatus);
-        add(p,BorderLayout.SOUTH);
-        
-   }
-   
-   void setStatusLine(String stat){
-       lblStatus.setText(stat);
-   }
-   
-    private void initClass() {
-        //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setSize(1000,900);
-        //setLocationRelativeTo(null); 
+        add(p, BorderLayout.SOUTH);
+    }
 
+    void setStatusLine(String stat) {
+        lblStatus.setText(stat);
+    }
+
+    private void initClass() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setSize(1000, 900);
+        setLocationRelativeTo(null);
+        mainContainer = getContentPane();
+        /*
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         me = this;
         // Capture app closing to close DB connection
         WindowListener exitListener = new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                /*
+                
                if (dbHelper !=null) {
                    dbHelper.closeDbConnection();
-                   
-
                 }
-*/
             System.exit(0);
             }
         };
-         me = this;       
+        me = this;       
         this.addWindowListener(exitListener);
-        mainContainer = getContentPane();
-       
+         */
     }
-    
-    
 }
